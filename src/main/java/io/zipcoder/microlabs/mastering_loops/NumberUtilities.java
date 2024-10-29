20558569b02b775e2be5c0db2f658c2734ed01f1
rjohnson0851 <ron21johnson@comcast.net>
2024-10-29 15:12:47 -0400

    done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -4,38 +4,63 @@ package io.zipcoder.microlabs.mastering_loops;
 public class NumberUtilities {
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int e = start; e < stop; e = e + step) {
+            result.append(Math.round(Math.pow(e,exponent)));
+        }
+        return result.toString();
     }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        return getExponentiations(start,stop,step,1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+
+        return getRange(start,stop, 1);
     }
 
     public static String getRange(int stop) {
-        return null;
+
+        return getRange(0,stop,1);
     }
 
     /*
      * natural break
      */
 
-    public static boolean isNumberEven(int toTest) { return false; }
-    public static boolean isNumberOdd(int toTest) { return false; }
+    public static boolean isNumberEven(int toTest) { return  ((toTest % 2) == 0); }
+    public static boolean isNumberOdd(int toTest) { return ((toTest % 2) != 0);  }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int e = start; e < stop; e = e + 1) {
+            if (isNumberEven(e))
+            {result.append(e);}
+        }
+        return result.toString();
+
+
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int e = start; e < stop; e = e + 1) {
+            if (isNumberOdd(e)) {
+
+                result.append(e);
+            }
+        }
+        return result.toString();
+
     }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 2);
     }
 
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,26 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+
+        for (int i = 1; i <= tableSize; i++){
+            for(int j = 1; j <= tableSize; j++) {
+                result.append(String.format("%3d |", (i * j)));
+
+
+            } result.append("\n");
+        }
+        return result.toString();
     }
 
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 }
